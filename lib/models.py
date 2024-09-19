be93f414536514ea2e014fdfe2325b1afb3827d8
Silvanos Eric <ericoderito@gmail.com>
2024-09-19 15:19:48 +0300

    feat: add Freebie model and seed script
    - Add Faker package to Pipfile
    - Create Alembic migration for Freebie model
    - Define Freebie model in models.py
    - Implement seed script to generate fake data for Freebie model

--- lib/models.py
+++ lib/models.py
@@ -9,6 +9,7 @@ metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
 
+
 class Company(Base):
     __tablename__ = 'companies'
 
@@ -19,11 +20,26 @@ class Company(Base):
     def __repr__(self):
         return f'<Company {self.name}>'
 
+
 class Dev(Base):
     __tablename__ = 'devs'
 
     id = Column(Integer(), primary_key=True)
-    name= Column(String())
+    name = Column(String())
 
     def __repr__(self):
         return f'<Dev {self.name}>'
+
+
+class Freebie(Base):
+    # Mapped table name at the database level
+    __tablename__ = 'freebies'
+
+    # Column definitions
+    id = Column(Integer(), primary_key=True)
+    item_name = Column(String(), nullable=False)
+    value = Column(Integer(), nullable=False)
+
+    # Foreign Keys definitions
+    dev_id = Column(Integer(), ForeignKey('devs.id'))
+    company_id = Column(Integer(), ForeignKey('companies.id'))
--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,66 @@
 #!/usr/bin/env python3
 
-# Script goes here!
+# This is a script to generate some fake swag data to be used in the
+# freebies database.
+
+# We import Faker, a Python package that generates fake data.
+from faker import Faker
+
+# We import random to generate random numbers.
+import random
+
+# We import the required classes from sqlalchemy.
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# We import the Freebie class from the models module.
+from models import Freebie
+
+if __name__ == '__main__':
+    # We create an engine object that represents the core interface to
+    # the database.
+    engine = create_engine('sqlite:///freebies.db')
+
+    # We create a sessionmaker object that creates new Session objects
+    # bound to the engine object.
+    Session = sessionmaker(bind=engine)
+
+    # We create a new session object from the sessionmaker.
+    session = Session()
+
+    # We delete all the freebies in the database.
+    session.query(Freebie).delete()
+
+    # We create a fake object that can be used to generate fake data.
+    fake = Faker()
+
+    # We create a list of swag items that we can randomly select from.
+    swag_items = [
+        "T-shirt", "Hoodie", "Sticker", "Notebook", "Pen", "USB Drive",
+        "Water Bottle", "Keychain", "Mouse Pad", "Lanyard", "Tote Bag",
+        "Sunglass", "Stress Ball", "Phone Stand", "Charging Cable",
+        "Power Bank", "Laptop Sleeve", "Coffee Mug", "Earbud", "Hat",
+        "Wristband", "Post-it Note", "Screen Cleaner", "Bottle Opener",
+        "Phone Grip", "Badge", "Button", "Puzzle", "Flashlight", "Umbrella"
+    ]
+
+    # We create a list to hold all the freebie objects that we create.
+    freebies = []
+
+    # We loop 30 times to create 30 freebie objects.
+    for _ in range(30):
+        # We create a new freebie object with a random swag item and a
+        # value between 1 and 3000.
+        freebie = Freebie(item_name=random.choice(swag_items),
+                          value=random.randint(1, 3000))
+        # We append the freebie object to the list of freebies.
+        freebies.append(freebie)
+
+    # We add the freebies to the session.
+    session.bulk_save_objects(freebies)
+
+    # We commit the session.
+    session.commit()
+
+    # We close the session.
+    session.close()
