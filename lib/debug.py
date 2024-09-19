9f428a27cde89c04d896a0a5ae9904ecc2d1094e
Silvanos Eric <ericoderito@gmail.com>
2024-09-19 20:34:50 +0300

    feat(debug): enhance debug script with session management and queries
    Added session management using SQLAlchemy's sessionmaker to the `debug.py`. Included queries to:
    1. Retrieve and print the company name for the first freebie.
    2. Retrieve and print a collection of all freebies for the first company.

--- lib/debug.py
+++ lib/debug.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
 
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-from models import Company, Dev
+from models import Company, Dev, Freebie
 
 if __name__ == '__main__':
     engine = create_engine('sqlite:///freebies.db')
-    import ipdb; ipdb.set_trace()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # 1. Freebie.company - return the Company instance for a Freebie object
+    freebie = session.query(Freebie).first()
+    print('1. Company name for the first freebie:', freebie.company)
+
+    # 2. Company.freebies - returns a collection of all freebies for the company
+    company = session.query(Company).first()
+    print('2. Collection of all freebies for first company', company.freebies)
+
+    session.close()
+    import ipdb
+    ipdb.set_trace()
